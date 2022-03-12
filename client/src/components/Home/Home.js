c9ac5d2cbb716c8d0b75ab49faa87892144fbbbb
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-03-12 15:18:54 -0500

    added the api calls for the state

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,46 +1,64 @@
-import { React, Component } from 'react';
-import './Home.scss';
-import City from '../City';
-import axios from 'axios';
-let apiKey = 'UcmWA5pipNdbXRVDpPVdtgA7qBuniPCb'
+import { React, Component } from "react";
+import "./Home.scss";
+import City from "../City";
+import axios from "axios";
+let apiKey = "UcmWA5pipNdbXRVDpPVdtgA7qBuniPCb";
+const SEARCH_URL =
+  "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+const WEATHER_URL =
+  "http://dataservice.accuweather.com/forecasts/v1/daily/1day/";
 
 export default class Home extends Component {
+  state = {
+    cityId: "",
+  };
 
-    state = {
-        cityId: "",
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target.city.value);
+    this.fetchCityID(e.target.city.value);
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e)
-        console.log(e.target.city.value)
-        this.fetchCityWeather(e.target.city.value)
-    }
+  fetchCityID(city) {
+    axios
+      .get(SEARCH_URL + "?apikey=" + apiKey + "&q=" + city)
+      .then((results) => {
+        const filteredResults = results.data[0];
 
-    fetchCityWeather(city) {
-        axios.get()
-        .then(results => {
-            console.log(results)
-            // this.setState({
-            //     cityId:
-            // })
-        })
-    }
+        console.log(filteredResults.Key);
+        console.log(results);
 
-    render() {
-        //console.log(props.routerProps)
-        console.log('home rendering')
-        console.log(this.state.city)
-        return (
-            <div>
-                <h1>Search For Your City's Weather</h1>
-                <form onSubmit = {(e) => this.handleSubmit(e)} >
-                    <input type="text" name="city" id="city" placeholder="Search your city"></input>
-                    <button>Search</button>
-                </form>
-                <City
-                    routeProps={this.props}/>
-            </div>
-        )
-    }
+        axios
+          .get(WEATHER_URL + filteredResults.Key + "?apikey=" + apiKey)
+          .then((results) => {
+            console.log(results.data.DailyForecasts[0].Temperature);
+          });
+        // this.setState({
+        //     cityId:
+        // })
+      });
+  }
+
+  render() {
+    //console.log(props.routerProps)
+    console.log("home rendering");
+    console.log(this.state.city);
+    return (
+      <div>
+        <h1>Search For Your City's Weather</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="Search your city"
+          ></input>
+          <button>Search</button>
+        </form>
+        <City routeProps={this.props} />
+      </div>
+    );
+  }
 }
